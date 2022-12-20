3eb16dd10702f8e250d52712b335d29f3595bd20
Matteo Rania <matteo.rania2014@gmail.com>
2022-12-20 10:28:50 +0100

    to do list data update

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const state = {
     users: [],
     posts: [],
     data:[],
-    _data: []
+    _data: [],
+    pageInfo: {
+        totalItems: 0,
+        totalPages: 1,
+        currentPage: 2,
+        limit: 10,
+        hasPrevPage: false,
+        hasNextPage: false,
+    }
 }
 
  const usersJson = async ()=> {
@@ -28,9 +36,30 @@ const state = {
     }
 }
 
+const formatData = () => {
+   state._data = state.posts.map(post => {
+    const user = state.users.find((user) => user.id == post.userId);
+    return {...post, author : user.name }
+   })
+}
+
+const applyPageIndex = () =>{
+    state.pageInfo.totalItems = state._data.length;
+    state.pageInfo.totalPages = Math.floor(state.pageInfo.totalItems / state.pageInfo.limit);
+    state.pageInfo.hasPrevPage = state.pageInfo.currentPage > 1;
+    state.pageInfo.hasNextPage = state.pageInfo.currentPage < state.pageInfo.totalPages;
+
+    const startIndex = state.pageInfo.limit * (state.pageInfo.currentPage - 1);
+
+    state.data = [...state._data].splice(startIndex, state.pageInfo.limit)
+} 
+
+
 const init = async ()=> {
      await usersJson();
-     await postJson()
+     await postJson();
+        formatData();
+        applyPageIndex(); 
      console.log(state)
 }
 
